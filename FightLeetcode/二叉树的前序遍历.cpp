2d7c05d91a200123d61b9fd0bf1157d9777c31c9
qiujunlin <1757591067@qq.com>
2020-09-28 20:39:20 +0800

    二叉树

---
+++ FightLeetcode/二叉树的前序遍历.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode* > sta;
+        vector<int> res;
+        if (root == NULL)  return res;
+        sta.push(root);
+        while (sta.size()) {
+            TreeNode* treenode = sta.top();
+            res.push_back(treenode->val);
+            sta.pop();
+            if (treenode->right) {
+                sta.push(treenode->right);
+            }
+            if (treenode->left) {
+                sta.push(treenode->left);                 
+            }
+        }
+
+        return  res;
+    }
+};
